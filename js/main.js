f5adc487448a99a416fdee7e4e87d0e380164215
Leonid Feskov <leonik89@gmail.com>
2017-03-31 13:20:55 +0300

    Add localstorage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 (function(){
     'use strict';
 
-    var cellSize = 30;
-    var deviation = Math.round(cellSize / 5);
+    var CELL_SIZE = 30;
+    var DIVIATION = Math.round(CELL_SIZE / 5);
+    var COLOR_GRID = '#EEEEEE';
+    var COLOR_STROKE = '#212121';
+
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
     var canvasGrid = document.getElementById('cubes-grid');
     var ctxGrid = canvasGrid.getContext('2d');
@@ -10,105 +16,174 @@
     var canvas = document.getElementById('cubes-canvas');
     var ctx = canvas.getContext('2d');
 
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+    canvas.width = canvasGrid.width = width;
+    canvas.height = canvasGrid.height = height;
 
-    canvasGrid.width = width;
-    canvasGrid.height = height;
-    canvas.width = width;
-    canvas.height = height;
+    ctxGrid.strokeStyle = COLOR_GRID;
+    ctx.strokeStyle = COLOR_STROKE;
 
-    ctxGrid.strokeStyle = '#EEEEEE';
-    ctxGrid.lineWidth = 1;
-    ctx.strokeStyle = '#212121';
-    ctx.lineWidth = 1;
+    var cellsCountByX = Math.floor(width / CELL_SIZE) + 1;
+    var cellsCountByY = Math.floor(height / CELL_SIZE) + 2;
 
-    function Cell() {
-        this.top = false;
-        this.bottom = false;
-        this.diagonal = false;
+
+    // Здесь храним рисунок
+    // userImage.cells - двумерный массив всех клеток. Каждая клетка содержит в себе информацию о нарисованном штрихе
+    // Чтобы общая грань двух соседних клеток не рисовалась дважды, рисуем только левую, верхнюю и диагональную линии для каждой клетки
+    var userImage;
+    if (localStorage.getItem('userImage')) {
+        drawFromStorage();
+    } else {
+        createEmptyUserImage();
     }
 
-    var userImage = {
-        cells: []
-    }
+    function createEmptyUserImage() {
+        userImage = {
+            cells: []
+        };
 
-    // рисуем сетку
-    // горизонтальные линии
-    ctxGrid.beginPath();
-    var i = 0;
-    while (cellSize * i <= height) {
-        ctxGrid.moveTo(0, cellSize * i);
-        ctxGrid.lineTo(width, cellSize * i);
-        i++;
-    }
-
-    // вертикальные линии
-    var j = 0;
-    while (cellSize * j <= width) {
-        ctxGrid.moveTo(cellSize * j, 0);
-        ctxGrid.lineTo(cellSize * j, height);
-        j++;
-    }
-    ctxGrid.closePath();
-    ctxGrid.stroke();
-
-    function getCellNumber(x, y) {
-        var posX = Math.floor(x / cellSize);
-        var posY = Math.floor(y / cellSize);
-        return {
-            x: posX,
-            y: posY,
-            coordX: posX * cellSize,
-            coordY: posY * cellSize
+        for (var i = 0; i < cellsCountByY; i++) {
+            userImage.cells[i] = [];
+            for (var j = 0; j < cellsCountByX; j++) {
+                userImage.cells[i][j] = null;
+            }
         }
     }
 
-    function drawDiagonal(cellNumber) {
-        ctx.beginPath();
-        ctx.moveTo(cellSize * cellNumber.x, cellSize * (cellNumber.y + 1));
-        ctx.lineTo(cellSize * (cellNumber.x + 1), cellSize * cellNumber.y);
-        ctx.closePath();
-        ctx.stroke();
+    // рисуем сетку
+    function drawGrid() {
+        // горизонтальные линии
+        ctxGrid.beginPath();
+        var i = 0;
+        while (CELL_SIZE * i <= height) {
+            ctxGrid.moveTo(0, CELL_SIZE * i);
+            ctxGrid.lineTo(width, CELL_SIZE * i);
+            i++;
+        }
+
+        // вертикальные линии
+        var j = 0;
+        while (CELL_SIZE * j <= width) {
+            ctxGrid.moveTo(CELL_SIZE * j, 0);
+            ctxGrid.lineTo(CELL_SIZE * j, height);
+            j++;
+        }
+        ctxGrid.closePath();
+        ctxGrid.stroke();
     }
 
-    function drawTop(cellNumber) {
-        ctx.beginPath();
-        ctx.moveTo(cellSize * cellNumber.x, cellSize * cellNumber.y);
-        ctx.lineTo(cellSize * (cellNumber.x + 1), cellSize * cellNumber.y);
-        ctx.closePath();
-        ctx.stroke();
+    drawGrid();
+
+    function getCellNumber(x, y) {
+        var posX = Math.floor(x / CELL_SIZE);
+        var posY = Math.floor(y / CELL_SIZE);
+        return {
+            x: posX,
+            y: posY,
+            coordX: posX * CELL_SIZE,
+            coordY: posY * CELL_SIZE
+        }
     }
 
-    function drawBottom(cellNumber) {
-        ctx.beginPath();
-        ctx.moveTo(cellSize * cellNumber.x, cellSize * (cellNumber.y + 1));
-        ctx.lineTo(cellSize * (cellNumber.x + 1), cellSize * (cellNumber.y + 1));
-        ctx.closePath();
-        ctx.stroke();
+    function addStrokeToStorage(cellNumber, strokeName) {
+        var cell = userImage.cells[cellNumber.y][cellNumber.x];
+        if (cell === null) {
+            userImage.cells[cellNumber.y][cellNumber.x] = {};
+        }
+        userImage.cells[cellNumber.y][cellNumber.x][strokeName] = true;
+
+        localStorage.setItem('userImage', JSON.stringify(userImage));
     }
 
-    function drawLeft(cellNumber) {
-        ctx.beginPath();
-        ctx.moveTo(cellSize * cellNumber.x, cellSize * cellNumber.y);
-        ctx.lineTo(cellSize * cellNumber.x, cellSize * (cellNumber.y + 1));
-        ctx.closePath();
-        ctx.stroke();
+    function clearCellInStorage(cellNumber) {
+        userImage.cells[cellNumber.y][cellNumber.x] = null;
+        var siblingRightCell = userImage.cells[cellNumber.y][cellNumber.x + 1];
+        var siblingBottomCell = userImage.cells[cellNumber.y + 1][cellNumber.x];
+
+        if (siblingRightCell !== null) {
+            delete userImage.cells[cellNumber.y][cellNumber.x + 1].left;
+            if (!siblingRightCell.top && !siblingRightCell.diagonal) {
+                userImage.cells[cellNumber.y][cellNumber.x + 1] = null;
+            }
+        }
+
+        if (siblingBottomCell !== null) {
+            delete userImage.cells[cellNumber.y + 1][cellNumber.x].top;
+            if (!siblingBottomCell.left && !siblingBottomCell.diagonal) {
+                userImage.cells[cellNumber.y + 1][cellNumber.x] = null;
+            }
+        }
+
+        localStorage.setItem('userImage', JSON.stringify(userImage));
     }
 
-    function drawRight(cellNumber) {
+    function drawDiagonal(cellNumber, save) {
         ctx.beginPath();
-        ctx.moveTo(cellSize * (cellNumber.x + 1), cellSize * cellNumber.y);
-        ctx.lineTo(cellSize * (cellNumber.x + 1), cellSize * (cellNumber.y + 1));
+        ctx.moveTo(CELL_SIZE * cellNumber.x, CELL_SIZE * (cellNumber.y + 1));
+        ctx.lineTo(CELL_SIZE * (cellNumber.x + 1), CELL_SIZE * cellNumber.y);
         ctx.closePath();
         ctx.stroke();
+        if (save) {
+            addStrokeToStorage(cellNumber, 'diagonal');
+        }
+    }
+
+    function drawTop(cellNumber, save) {
+        ctx.beginPath();
+        ctx.moveTo(CELL_SIZE * cellNumber.x, CELL_SIZE * cellNumber.y);
+        ctx.lineTo(CELL_SIZE * (cellNumber.x + 1), CELL_SIZE * cellNumber.y);
+        ctx.closePath();
+        ctx.stroke();
+        if (save) {
+            addStrokeToStorage(cellNumber, 'top');
+        }
+    }
+
+    function drawLeft(cellNumber, save) {
+        ctx.beginPath();
+        ctx.moveTo(CELL_SIZE * cellNumber.x, CELL_SIZE * cellNumber.y);
+        ctx.lineTo(CELL_SIZE * cellNumber.x, CELL_SIZE * (cellNumber.y + 1));
+        ctx.closePath();
+        ctx.stroke();
+        if (save) {
+            addStrokeToStorage(cellNumber, 'left');
+        }
     }
 
     function clearCell(cellNumber) {
-        ctx.clearRect(cellNumber.coordX - 1, cellNumber.coordY - 1, cellSize + 2, cellSize + 2);
+        ctx.clearRect(cellNumber.coordX - 1, cellNumber.coordY - 1, CELL_SIZE + 2, CELL_SIZE + 2);
+        clearCellInStorage(cellNumber);
     }
 
-    document.addEventListener('click', function(event) {
+    function drawFromStorage () {
+        userImage = JSON.parse(localStorage.getItem('userImage'));
+        for (var i = 0; i < userImage.cells.length; i++) {
+            var row = userImage.cells[i];
+            for (var j = 0; j < row.length; j++) {
+                var cell = row[j];
+                var cellNumber = {
+                    x: j,
+                    y: i,
+                    coordX: j * CELL_SIZE,
+                    coordY: i * CELL_SIZE
+                };
+                if (cell === null) {
+                    continue;
+                }
+                if (cell.left) {
+                    drawLeft(cellNumber, false);
+                }
+                if (cell.top) {
+                    drawTop(cellNumber, false);
+                }
+                if (cell.diagonal) {
+                    drawDiagonal(cellNumber, false);
+                }
+            }
+        }
+
+    }
+
+    canvas.addEventListener('click', function(event) {
         var x = event.clientX;
         var y = event.clientY;
 
@@ -118,21 +193,39 @@
         var deltaX = x - cellNumber.coordX;
         var deltaY = y - cellNumber.coordY;
 
-        if (event.ctrlKey) {
+        if (event.altKey) {
             clearCell(cellNumber);
             return;
         }
 
-        if (deltaX <= deviation) {
-            drawLeft(cellNumber);
-        } else if (deltaY <= deviation) {
-            drawTop(cellNumber);
-        } else if (deltaX >= cellSize - deviation) {
-            drawRight(cellNumber);
-        } else if (deltaY >= cellSize - deviation) {
-            drawBottom(cellNumber);
+        if (deltaX <= DIVIATION) {
+            drawLeft(cellNumber, true);
+        } else if (deltaY <= DIVIATION) {
+            drawTop(cellNumber, true);
+        } else if (deltaX >= CELL_SIZE - DIVIATION) {
+            cellNumber.x += 1;
+            cellNumber.coordX += CELL_SIZE;
+            drawLeft(cellNumber, true);
+        } else if (deltaY >= CELL_SIZE - DIVIATION) {
+            cellNumber.y += 1;
+            cellNumber.coordY += CELL_SIZE;
+            drawTop(cellNumber, true);
         } else {
-            drawDiagonal(cellNumber);
+            drawDiagonal(cellNumber, true);
         }
+    });
+
+    var buttonClearAll = document.querySelector('.js-clear-all');
+    var buttonSaveImage = document.querySelector('.js-save-image');
+
+    buttonClearAll.addEventListener('click', function() {
+        ctx.clearRect(0, 0, width, height);
+        localStorage.clear();
+        createEmptyUserImage();
+    });
+
+    buttonSaveImage.addEventListener('click', function() {
+        var dataURL = canvas.toDataURL();
+        window.open(dataURL);
     });
 })();
